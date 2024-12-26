ad6ae518420fef49ef1fb6ca7567954d28e9faf9
AcSunday <1312192622@qq.com>
2024-12-26 18:09:50 +0800

    add tron util

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -14,3 +14,14 @@ func TestDecimal(t *testing.T) {
 	t.Log(d)
 	t.Log(d.String())
 }
+
+func TestTronAddrConvert(t *testing.T) {
+	addr := "35c63937d1165efa511f57838b050b9612e4b4a9"
+	newAddr, err := Tron.FromHexAddress(addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(newAddr)
+	tronAddr := "TEsYH363FySAj9UtDuc3ee6chU8DTLQqr3"
+	t.Log(Tron.ToHexAddress(tronAddr))
+}
